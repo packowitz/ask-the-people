d1be118aa438c7b0f008b116fd44d4955d2212aa
Philipp Schumacher <p.schumacher@auckland.ac.nz>
2016-06-27 22:24:26 +1200

    icon and splashscreen

--- app/pages/loading/loading.ts
+++ app/pages/loading/loading.ts
@@ -1,6 +1,11 @@
 import {Component} from "@angular/core";
+import {Splashscreen} from "ionic-native/dist/index";
 
 @Component({
   templateUrl: 'build/pages/loading/loading.html'
 })
-export class LoadingPage {}
+export class LoadingPage {
+  ionViewDidEnter() {
+    Splashscreen.hide();
+  }
+}
