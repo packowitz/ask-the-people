ba759a162d2ded7658997a186f646e9b04a89928
Philipp Schumacher <p.schumacher@auckland.ac.nz>
2016-08-31 10:40:03 +1200

    login

--- app/pages/welcome/welcome.ts
+++ app/pages/welcome/welcome.ts
@@ -1,6 +1,5 @@
 import {Component} from "@angular/core";
 import {NavController, LoadingController, ToastController, Storage, SqlStorage} from "ionic-angular";
-import {LoginPage} from "../login/login";
 import {Model} from "../../components/model.component";
 import {AuthService} from "../../services/auth.service";
 import {LoadingPage} from "../loading/loading";
@@ -9,15 +8,20 @@ import {LoadingPage} from "../loading/loading";
   templateUrl: 'build/pages/welcome/welcome.html'
 })
 export class WelcomePage {
+  showLogin: boolean = false;
+  username: string;
+  password: string;
 
   constructor(private nav: NavController,
               private model: Model,
               private authService: AuthService,
               private loadingController: LoadingController,
-              private toastController: ToastController) {}
+              private toastController: ToastController) {
+  }
 
-  gotoLogin() {
-    this.nav.setRoot(LoginPage);
+  toggleShowLogin() {
+    console.log(this.username + " - " + this.password);
+    this.showLogin = !this.showLogin;
   }
 
   register() {
@@ -44,6 +48,27 @@ export class WelcomePage {
         );
       }
     );
+  }
+
+  login() {
+    let loading = this.loadingController.create({
+      content: 'Logging in',
+      spinner: 'dots'
+    });
+    loading.present();
+    this.authService.login(this.username, this.password).subscribe(data => {
+      this.model.token = data.token;
+      this.model.user = data.user;
+      let storage = new Storage(SqlStorage);
+      storage.set('token', data.token).then(() => loading.dismiss().then(() => this.nav.setRoot(LoadingPage)));
+    }, error => {
+      loading.dismiss().then(() => {
+        this.toastController.create({
+          message: "Login not successful",
+          duration: 2000
+        }).present();
+      });
+    });
   }
 
 }
